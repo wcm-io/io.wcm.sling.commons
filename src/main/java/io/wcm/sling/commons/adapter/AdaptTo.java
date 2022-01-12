a861bb015cee20d60b767ff70026adf398f243c1
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-01-12 22:28:18 +0100

    test commit for PR

--- src/main/java/io/wcm/sling/commons/adapter/AdaptTo.java
+++ src/main/java/io/wcm/sling/commons/adapter/AdaptTo.java
@@ -29,6 +29,9 @@ import org.osgi.annotation.versioning.ProviderType;
 @ProviderType
 public final class AdaptTo {
 
+  public static final int const_with_invalid_name = 0;
+  public int VARIBLE_WITH_INVALD_NAME = 0;
+
   private AdaptTo() {
     // static methods only
   }
