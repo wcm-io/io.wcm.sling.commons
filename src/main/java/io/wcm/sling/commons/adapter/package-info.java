4d86730cd72eb3d47514b64705acdb8a27b8a5ca
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-01-12 22:31:56 +0100

    fix package version

--- src/main/java/io/wcm/sling/commons/adapter/package-info.java
+++ src/main/java/io/wcm/sling/commons/adapter/package-info.java
@@ -20,5 +20,5 @@
 /**
  * Helper classes around the Sling {@link org.apache.sling.api.adapter.Adaptable} concept.
  */
-@org.osgi.annotation.versioning.Version("1.0.1")
+@org.osgi.annotation.versioning.Version("1.3")
 package io.wcm.sling.commons.adapter;
